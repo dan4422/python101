f2f57a46cc0b11d06138b1e5a7200e9c3147c06c
Daniel Lee <sdlee095@gmail.com>
2022-04-11 21:30:38 -0400

    added finishing touches to to-do hw

--- to-do-list.py
+++ to-do-list.py
@@ -12,24 +12,21 @@ press q to quit:
     
     if user_input == '1':
         title = input('What is the title of the task?: ')
-        priority = input('What level priority?(high/medium/low): ')
-        if priority != 'high' or priority != 'medium' or priority != 'low':
+        priority = input('What level priority?(high/medium/low): ').lower()
+        if (priority == 'high') or (priority == 'medium') or (priority == 'low'):
+            entered_task = {'title': title, 'priority':priority}
+            tasks.append(entered_task)
+        else:
             print('Use only high, medium, or low!')
-        entered_task = {'title': title,
-        'priority':priority}
-        tasks.append(entered_task)
     elif user_input == '2': #deletes a task based on index number
-        for task in tasks:
-            print(f"{tasks.index(task)} - {task['title']} - {task['priority']}")
+        list_of_tasks(tasks)
         user_delete = int(input('Enter the number of the task to delete: '))
         del tasks[user_delete]
         print('Remaining tasks:')
-        for task in tasks:
-            print(f"{tasks.index(task)} - {task['title']} - {task['priority']}")
+        list_of_tasks(tasks)
     elif user_input == '3':
         #view all the tasks by index number then title then priority
-        for task in tasks:
-            print(f"{tasks.index(task)} - {task['title']} - {task['priority']}")
+        list_of_tasks(tasks)
     elif user_input == 'q':
         break
     else:
